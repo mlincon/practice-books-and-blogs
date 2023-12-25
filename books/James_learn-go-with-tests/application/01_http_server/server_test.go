60db034e89cf4f6c16175d08aa90d4674a3404e6
M Lincon <49597604+mlincon@users.noreply.github.com>
2023-12-25 21:17:34 +0000

    james-go: http server

---
+++ books/James_learn-go-with-tests/application/01_http_server/server_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayers(t *testing.T) {
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request := getScoreRequest("Pepper")
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "20")
+	})
+
+	t.Run("return Floyd's score", func(t *testing.T) {
+		request := getScoreRequest("Floyd")
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "10")
+	})
+}
+
+func getScoreRequest(name string) *http.Request {
+	request, _ := http.NewRequest(
+		http.MethodGet,
+		fmt.Sprintf("/players/%s", name),
+		nil,
+	)
+	return request
+}
+
+func assertResponseBody(t testing.TB, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q want %q", got, want)
+	}
+}
