a180b22a3950e83e190db42a74fcf9aee48a826c
M Lincon <49597604+mlincon@users.noreply.github.com>
2024-01-03 23:23:13 +0000

    simple start to llama index

---
+++ courses/Marco-LlamaIndex/main.py
@@ -0,0 +1,18 @@
+from llama_index import VectorStoreIndex
+from llama_index.readers import SimpleWebPageReader
+
+
+def main(urls: list[str]) -> None:
+    documents = SimpleWebPageReader(html_to_text=True).load_data(urls=urls)
+    index = VectorStoreIndex.from_documents(documents=documents)
+    query_engine = index.as_query_engine()
+    response = query_engine.query("What is LlamaIndex?")
+    print(response)
+
+
+if __name__ == "__main__":
+    main(
+        urls=[
+            "https://cbarkinozer.medium.com/an-overview-of-the-llamaindex-framework-9ee9db787d16",
+        ]
+    )
--- courses/Marco-LlamaIndex/readme.md
+++ courses/Marco-LlamaIndex/readme.md
@@ -1,2 +1,8 @@
 Udemy Course:
 LlamaIndex - Develop LLM powered applications with LlamaIndex by Eden Marco
+
+### Update requirements.txt
+
+```
+pip list --format=freeze > requirements.txt
+```
