e1e76a51dbc119a2c38a500ea382aa7456d4d787
M Lincon <49597604+mlincon@users.noreply.github.com>
2024-01-03 22:11:07 +0000

    fix order of conda commands

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,10 +3,8 @@
 # install pre-commit
 pre-commit install
 
-# prepare shell for condas
-conda init
-
 # create and install libraries for Llama Index
 conda create -y --name llamaIndex python=3.11
+conda init
 conda activate llamaIndex && \
     pip install -r courses/Marco-LlamaIndex/requirements.txt
