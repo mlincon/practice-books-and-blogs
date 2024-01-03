7ee226a2506eba5dbcab008e62f71f796a8501c4
M Lincon <49597604+mlincon@users.noreply.github.com>
2024-01-03 21:57:46 +0000

    start adding files to the project

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,5 +3,10 @@
 # install pre-commit
 pre-commit install
 
-# pull latest changes
-git pull --rebase --all
+# prepare shell for condas
+conda init
+
+# create and install libraries for Llama Index
+conda create -y --name llamaIndex python=3.11
+conda activate llamaIndex && \
+    pip install -r courses/Marco-LlamaIndex/requirements.txt
