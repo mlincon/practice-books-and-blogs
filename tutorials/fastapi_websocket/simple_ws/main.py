54bb4934da9e467f53eeef1278ead8c6b9f02c14
M Lincon <49597604+mlincon@users.noreply.github.com>
2024-01-14 12:54:03 +0000

    simple websocket application using fastapi

---
+++ tutorials/fastapi_websocket/simple_ws/main.py
@@ -0,0 +1,58 @@
+from typing import Annotated
+
+from fastapi import (
+    Cookie,
+    Depends,
+    FastAPI,
+    Query,
+    WebSocket,
+    WebSocketException,
+    status,
+)
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # can alter with time
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+async def get_cookie_or_token(
+    websocket: WebSocket,
+    session: Annotated[str | None, Cookie()] = None,
+    token: Annotated[str | None, Query()] = None,
+):
+    if session is None and token is None:
+        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
+    return session or token
+
+
+@app.websocket("/items/{item_id}/ws")
+async def websocket_endpoint(
+    *,
+    websocket: WebSocket,
+    item_id: str,
+    q: int | None = None,
+    cookie_or_token: Annotated[str, Depends(get_cookie_or_token)],
+):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(
+            f"Session cookie or query token value is: {cookie_or_token}"
+        )
+        if q is not None:
+            await websocket.send_text(f"Query parameter q is: {q}")
+        await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
+
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="debug", reload=True)
